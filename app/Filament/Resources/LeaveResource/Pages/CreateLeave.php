f890588f1da52afa5c2cb61e47c92179ee4425e3
Ariq Farhan <ariqalthaf445@gmail.com>
2025-04-28 21:52:27 +0700

    Add leave approval creation after leave record creation and password hashing before user record creation

--- app/Filament/Resources/LeaveResource/Pages/CreateLeave.php
+++ app/Filament/Resources/LeaveResource/Pages/CreateLeave.php
@@ -3,10 +3,24 @@
 namespace App\Filament\Resources\LeaveResource\Pages;
 
 use App\Filament\Resources\LeaveResource;
+use App\Models\LeaveApproval;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateLeave extends CreateRecord
 {
     protected static string $resource = LeaveResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    public function afterCreate(): void
+    {
+        LeaveApproval::create([
+            'leave_id' => $this->record->id,
+            'status' => 'pending',
+        ]);
+    }
 }
