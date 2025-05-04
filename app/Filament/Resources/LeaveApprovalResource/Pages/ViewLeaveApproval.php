c919b92db6d4924652948e0f821ce41faa9f403a
Ariq Farhan <ariqalthaf445@gmail.com>
2025-05-04 17:31:01 +0700

    Comment out EditAction in ViewLeaveApproval header actions

--- app/Filament/Resources/LeaveApprovalResource/Pages/ViewLeaveApproval.php
+++ app/Filament/Resources/LeaveApprovalResource/Pages/ViewLeaveApproval.php
@@ -18,7 +18,7 @@ class ViewLeaveApproval extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\EditAction::make(),
+            // Actions\EditAction::make(),
         ];
     }
 
